b9db267b322ab7984f23e248ed4ecc2ca5a94254
liavergou <liaverg@gmail.com>
2025-10-24 23:23:49 +0300

    feat: add Footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,24 +1,16 @@
-import { Box, Typography } from '@mui/material';
+const Footer = () =>{
+    const currentYear:number = new Date().getFullYear();
+    return(
 
-const Footer = () => {
-    return (
-        <Box
-            component="footer"
-            sx={{
-                bgcolor: 'grey.800',
-                color: 'white',
-                p: 2,
-                textAlign: 'center',
-                width: '100%',
-                mt: 'auto',
-            }}
-        >
-            <Typography variant="body2">
-                &copy; {new Date().getFullYear()} All rights reserved.
-            </Typography>
-            <Footer />
-        </Box>
-    );
-};
+        <>
+            <footer className="bg-gray-800 text-white">
+                <div>
+                    @{currentYear} Coding Factory 8. All Rights Reserved.
+                </div>
+            </footer>
+
+        </>
+    )
+}
 
 export default Footer;
