25d1fa33537f6f8b7ee489c361404a5375cea6a8
liavergou <liaverg@gmail.com>
2025-10-24 23:26:34 +0300

    feat: add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./layout/Dashboard.tsx";
-
+import HomePage from "./components/pages/HomePage.tsx";
+import Cropper from "./components/Cropper.tsx";
 function App() {
 
 
   return (
     <>
-        <Dashboard/>
+
+
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Dashboard/>}>
+                    <Route index element={<HomePage />}/>
+                    <Route path="/test-upload-area" element={<Cropper />}/>
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
 
     </>
   )
