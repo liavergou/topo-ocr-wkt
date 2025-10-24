a9468c472831160b3f8be522f987c63733144c4a
liavergou <liaverg@gmail.com>
2025-10-24 23:29:46 +0300

    feat: add UploadArea component and type

---
+++ src/components/UploadArea.tsx
@@ -0,0 +1,42 @@
+// Import των types που ορίζουν τη δομή των props του component
+import type { UploadAreaProps } from '../types';
+//https://claritydev.net/blog/react-typescript-drag-drop-file-upload-guide
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/file
+
+
+const UploadArea = ({
+                        onDragOver
+                        , onDragLeave
+                        , onDrop
+                        ,onFileChange
+                        , isDragging
+                    }: UploadAreaProps) => {
+    return (
+
+        <div
+            className={`flex items-center justify-center  border-5 border-dashed rounded-xl p-8 min-h-[400px]
+          ${isDragging ? 'border-blue-500 bg-blue-50' : 'border-gray-300'}`}
+
+            onDragOver={onDragOver}    // Ενεργοποιείται όταν σύρουμε πάνω από το element
+            onDragLeave={onDragLeave}  // Ενεργοποιείται όταν φεύγουμε από το element
+            onDrop={onDrop}            // Ενεργοποιείται όταν αφήνουμε το αρχείο
+        >
+            <div className="text-center">
+
+                <label className="px-6 py-3 bg-slate-700 text-white">
+                    {/*
+
+                    */}
+                    <input
+                        type="file"
+                        accept="image/*,application/pdf"
+                        onChange={onFileChange}
+
+                    />
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default UploadArea;
--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,12 @@ export type MenuItemProps = {
     path: string;
     icon: React.ReactNode;
 };
+
+//https://developer.mozilla.org/en-US/docs/Web/API/DragEvent
+export type UploadAreaProps = {
+    onDragOver: (e: React.DragEvent<HTMLDivElement>) => void;
+    onDragLeave: (e: React.DragEvent<HTMLDivElement>) => void;
+    onDrop: (e: React.DragEvent<HTMLDivElement>) => void;
+    onFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    isDragging: boolean;
+};
