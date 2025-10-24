43902df02c555af31030d9f8cab677cf8fc2fc43
liavergou <liaverg@gmail.com>
2025-10-25 00:24:00 +0300

    feat: add MenuItemProps, add navigation
    styling: update icons

--- src/layout/Dashboard.tsx
+++ src/layout/Dashboard.tsx
@@ -10,15 +10,34 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import Footer from "./Footer.tsx";
-import { Outlet } from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
+import type {MenuItemProps} from "../types.ts";
+import HomeIcon from '@mui/icons-material/Map';
+import FolderIcon from '@mui/icons-material/Folder';
+import CropIcon from '@mui/icons-material/Crop';
+
 
 //https://mui.com/material-ui/react-drawer/#permanent-drawer
 
 const Dashboard =   ()=> {
     const drawerWidth = 250;
+
+    const menuItems: MenuItemProps[] = [
+        { label: 'Αρχική', icon: <HomeIcon />, path: '/' },
+        { label: 'Επεξεργασία Τοπογραφικών', icon: <CropIcon />, path: '/cropper' },
+        { label: 'Μελέτες', icon: <FolderIcon />, path: '/projects' },
+        { label: 'Διαχείριση Χρηστών', icon: <FolderIcon />, path: '/users' }
+    ];
+
+    const navigate = useNavigate();
+
+    const handleMenuClick = (path: string) => {
+        navigate(path);
+    };
+
+
+
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <CssBaseline />
@@ -48,30 +67,20 @@ const Dashboard =   ()=> {
                     <Toolbar />
                     <Divider />
                     <List>
-                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
+                        {menuItems.map((item) => (
+                            <ListItem key={item.label} disablePadding>
+                                <ListItemButton
+                                    onClick={() => handleMenuClick(item.path)}>
                                     <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        {item.icon}
                                     </ListItemIcon>
-                                    <ListItemText primary={text} />
+                                    <ListItemText primary={item.label} />
                                 </ListItemButton>
                             </ListItem>
                         ))}
                     </List>
                     <Divider />
-                    <List>
-                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
+
                 </Drawer>
                 <Box
                     component="main"
