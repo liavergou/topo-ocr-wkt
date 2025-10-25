d3864114f71d0ca19cb86391430687937aa35150
liavergou <liaverg@gmail.com>
2025-10-25 08:06:02 +0300

    feat: add intro image to HomePage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,18 +1,24 @@
-
 import {useEffect} from "react";
+import intro from "../../assets/img/intro.jpg";
+
 
 const HomePage = () => {
 
 
-    useEffect(()=> {
+    useEffect(() => {
         document.title = "CF8 Project";
     }, []);
 
     return (
         <>
 
-            test Home Page
+            <img
+                src={intro}
+                alt="Description"
+                className="w-auto max-w-full max-h-[70vh] mx-auto  rounded-lg shadow-2lg"
+            />
         </>
+
     )
 }
 export default HomePage;
