7c54dc00190a9cf4e58ffc642205579b257254d6
liavergou <liaverg@gmail.com>
2025-10-25 14:08:49 +0300

    feat: add conditional rendering for UploadArea and ImageToolbar. Add showToolbar state

--- src/components/pages/ProjectsPage.tsx
+++ src/components/pages/ProjectsPage.tsx
@@ -1,15 +1,14 @@
-import ImageToolbar from "../ImageToolbar.tsx";
 
 const ProjectsPage = () => {
     return (
         <div className="text-white p-8">
             <h1 className="text-3xl font-bold">Projects</h1>
 
-            <div className="mt-8 flex justify-center">
-                <ImageToolbar />
-            </div>
+            {/*<div className="mt-4 flex justify-center">*/}
+            {/*    <ImageToolbar />*/}
+            {/*</div>*/}
 
-            <p className="mt-4 text-gray-400"></p>
+            {/*<p className="mt-4 text-gray-400"></p>*/}
         </div>
     );
 };
