b862e31de0c973c9009c5bd9388a278d3fa7fbbe
liavergou <liaverg@gmail.com>
2025-10-26 05:59:58 +0200

    fix: update file input label and improve component styling

--- src/components/pages/UploadArea.tsx
+++ src/components/pages/UploadArea.tsx
@@ -15,26 +15,25 @@ const UploadArea = ({
         <div
             className={`flex items-center justify-center border-8 border-dashed rounded-xl p-8 min-h-[400px]
           ${isDragging ? 'border-blue-500 bg-blue-50' : 'border-gray-700 bg-gray-100'}`}
+            //conditional για αλλαγη χρώματος στο drag πανω απο το element
 
             onDragOver={onDragOver}    // Ενεργοποιείται όταν σύρουμε πάνω από το area
             onDragLeave={onDragLeave}  // Ενεργοποιείται όταν φεύγουμε από το area
             onDrop={onDrop}            // Ενεργοποιείται όταν αφήνουμε το αρχείο
+
         >
+
             <div className="text-center">
-
-                <label className="px-6 py-3 bg-slate-700 text-white">
-                    {/*
-
-                    */}
-                    <input
-                        type="file"
-                        accept="image/*,application/pdf"
-                        onChange={onFileChange}
-
-                    />
+                <p className="mt-2 mb-5 text-gray-500 font-semibold text-2xl">
+                    Drag & Drop ή
+                </p>
+                <label htmlFor="file-upload"
+                       className="px-3 py-3 cursor-pointer rounded-md bg-blue-600 hover:bg-blue-500  text-2xl font-semibold text-white shadow-sm ">
+                    Επιλέξτε αρχείο..
                 </label>
+
                 <input
-                    id="file-upload-dark"
+                    id="file-upload"
                     type="file"
                     accept="image/*,application/pdf,image/tiff"
                     onChange={onFileChange}
