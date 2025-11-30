9ed4e5dd426b809ac021d26beec903efc6c1d079
liavergou <liaverg@gmail.com>
2025-11-30 12:32:05 +0200

    fix: remove optional from properties description and jobs count. set description nullable.

--- src/schemas/projects.ts
+++ src/schemas/projects.ts
@@ -5,8 +5,8 @@ export const projectSchema = z.object({
     projectName: z.string() //κωδικός μελέτης
         .min(1, {error: "Ο κωδικός μελέτης είναι υποχρεωτικός"}),
         // .regex(/^[a-zA-Z0-9_-]+$/, {message: "Επιτρέπονται μόνο γράμματα, αριθμοί, - και _"}), //validation κωδικού
-    description: z.string().optional(),
-    jobsCount: z.coerce.number().int().optional()
+    description: z.string().nullable(),
+    jobsCount: z.coerce.number().int()
 })
 
 export type Project = z.infer<typeof projectSchema>;
