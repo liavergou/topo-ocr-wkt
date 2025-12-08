bf8bcb019c35cf947d7bd0cb928a88c212086d07
liavergou <liaverg@gmail.com>
2025-12-08 18:03:22 +0200

    feat(MapPreview): add leaflet map with layers (google satellite and ekxa wms) to display polygon from transformed coordinates and add circle pointers.

--- src/components/pages/MapPreview.tsx
+++ src/components/pages/MapPreview.tsx
@@ -1,8 +1,27 @@
-import { Box, Typography } from '@mui/material';
+import {Box, Typography} from '@mui/material';
 import type { MapPreviewProps } from '@/types';
 import 'leaflet/dist/leaflet.css';
+import {MapContainer, TileLayer, Polygon, WMSTileLayer, LayersControl, CircleMarker, Popup} from 'react-leaflet';
+import L, { latLngBounds } from 'leaflet';
+import { egsa87ToWgs84 } from '@/utils/ProjectionConverter';
+//https://react-leaflet.js.org/docs/start-introduction/
+//https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z} google satellite
+
+//<MapContainer>->Map Container
+//<TileLayer>->Tile basemap (Google)
+//<WMSTileLayer>->WMS layers (GeoServer)
+//<Popup>-> Popups
+//<Polygon>->Polygons
+
+
+//αλλαγές στον πίνακα συντεταγμένων τα πιάνει απο το handleChange του CoordinateTable->setCoordinates(updated)->useEffect του calculatedArea
 
 const MapPreview = ({ coordinates, area }: MapPreviewProps) => {
+
+    //μετατροπή σε lat lon με το util
+    const latLngs: [number, number][] = coordinates.map(c => egsa87ToWgs84(c.x, c.y)); //μετατροπη ΕΓΣΑ87 -> WGS84
+    //κεντράρισμα χάρτη
+    const bounds = latLngs.length > 0 ? latLngBounds(latLngs) : undefined;
 
     return (
         <Box
@@ -19,9 +38,59 @@ const MapPreview = ({ coordinates, area }: MapPreviewProps) => {
                 position: 'relative',
             }}
         >
-            <Typography variant="h6" color="text.secondary" gutterBottom>
-                Χάρτης
-            </Typography>
+            {/*ΧΑΡΤΗΣ MAP CONTAINER ΤΟΥ LEAFLET*/}
+            <MapContainer
+                style={{ height: '100%', width: '100%' }}
+                bounds={bounds}
+                boundsOptions={{ padding: [50, 50], maxZoom: 30 }}
+            >
+                {/* Layer Control - Radio buttons για basemaps */}
+                <LayersControl position="topright">
+
+                    {/*GOOGLE SATELLITE*/}
+                    <LayersControl.BaseLayer checked name="Google Satellite">
+                        <TileLayer
+                            url="https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}"
+                            maxZoom={30}
+                        />
+                    </LayersControl.BaseLayer>
+
+                    {/* ΕΚΧΑ Ορθοφωτοχάρτης */}
+                    <LayersControl.BaseLayer name="ΕΚΧΑ Ορθοφωτοχάρτης">
+                        <WMSTileLayer
+                            url="https://gis.ktimanet.gr/wms/wmsopen/wmsserver.aspx"
+                            maxZoom={30}
+                            maxNativeZoom={18}
+                            layers="ORTHOPHOTOS"
+                            format="image/png"
+                            transparent={true}
+                            crs={L.CRS.EPSG3857}
+                        />
+                    </LayersControl.BaseLayer>
+                </LayersControl>
+
+
+
+                <Polygon positions={latLngs}
+                pathOptions={{color:'#F5F02A', weight:4, fillOpacity: 0}}/>
+
+                {/* markers στις συντεταγμενες */}
+                {latLngs.map((point, index) => (
+                    <CircleMarker
+                        key={`point-${index}`}
+                        center={point}
+                        radius={3}
+                        pathOptions={{
+                            color: 'magenta',
+                            weight: 2
+                        }}
+                    >
+                        <Popup>Σημείο {index + 1}</Popup>
+                    </CircleMarker>
+                ))}
+
+
+            </MapContainer>
 
             {/*AREA*/}
             <Box
@@ -34,6 +103,7 @@ const MapPreview = ({ coordinates, area }: MapPreviewProps) => {
                     px: 1,
                     py: 1,
                     borderRadius: 1,
+                    zIndex: 1000, //για να κάτσει πανω απο τον χάρτη
                 }}
             >
                 <Typography variant="h6" sx={{ fontWeight: 'bold', color: 'secondary.light' }}>
