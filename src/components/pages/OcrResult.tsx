38cb369fb65d3b1a67925637147e1efe01d886aa
liavergou <liaverg@gmail.com>
2025-12-08 02:49:46 +0200

    feat(OcrResult): add parent component integrating coordinates table and map preview

---
+++ src/components/pages/OcrResult.tsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react';
+import { Box, Paper, Button } from '@mui/material';
+import MapPreview from './MapPreview';
+
+import type { Coordinate} from '@/types';
+import {calculatePolygonArea} from "@/utils/areaCalculator.ts";
+import CoordinatesTable from "@/components/pages/CoordinatesTable.tsx";
+
+
+type CoordinatesResultProps = {
+    initialCoordinates: Coordinate[];
+};
+
+const OcrResult = ({ initialCoordinates }: CoordinatesResultProps) => {
+
+    const [coordinates, setCoordinates] = useState<Coordinate[]>(initialCoordinates); //ΣΥΝΤΕΤΑΓΜΕΝΕΣ προσοχή source of truth για να το δώσει στα children. αλλάζει στο onChange των children
+    const [area, setArea] = useState(0); //ΕΜΒΑΔΟΝ
+
+    //Επανυπολογισμός εμβαδού όταν αλλάζει κάτι στις συντεταγμένες. προσοχη το state στο CoordinatesTable θα φέρει αλλαγή
+    useEffect(() => {
+        if (coordinates.length >= 3) {
+            const calculatedArea = calculatePolygonArea(coordinates);
+            setArea(calculatedArea);
+        }
+    }, [coordinates]);
+
+    const handleCoordinatesChange = (updated: Coordinate[]) => {
+        setCoordinates(updated);
+    };
+
+    const handleSave = () => {
+        // TODO: Save to backend
+        console.log('Saving coordinates:', coordinates);
+        alert('Αποθήκευση στο backend (TODO)');
+    };
+
+    const handleExportText = () => {
+        // TODO: Export TXT
+    };
+
+    return (
+
+        // ΧΑΡΤΗΣ
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4, height: '100%' }}>
+            <Paper variant="outlined" sx={{ p: 2 }}>
+                <MapPreview coordinates={coordinates} area={area} />
+            </Paper>
+
+        {/*ΠΙΝΑΚΑΣ ΣΥΝΤΕΤΑΓΜΕΝΩΝ*/}
+            <Paper variant="outlined" sx={{ p: 2}}>
+                <CoordinatesTable
+                    coordinates={coordinates}
+                    onChange={handleCoordinatesChange}
+                />
+
+                {/*ΑΠΟΘΗΚΕΥΣΗ ΣΕ ΠΕΡΙΠΤΩΣΗ ΑΛΛΑΓΗΣ*/}
+                <Box sx={{ display: 'flex', gap: 2, mt: 2 }}>
+                    <Button
+                        variant="contained"
+                        color="success"
+                        onClick={handleSave}
+                        fullWidth
+                    >
+                        ΑΠΟΘΗΚΕΥΣΗ
+                    </Button>
+
+                    {/*EXPORT TXT ΓΙΑ ΘΕΑΣΗ*/}
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={handleExportText}
+                        fullWidth
+                    >
+                        ΕΞΑΓΩΓΗ TXT
+                    </Button>
+                </Box>
+            </Paper>
+        </Box>
+    );
+};
+
+export default OcrResult;
+
+
