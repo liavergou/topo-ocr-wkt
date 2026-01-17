ff612392dcc94bb808686e1616fa121dd2ec9c44
liavergou <liaverg@gmail.com>
2026-01-17 15:27:14 +0200

    fix: remove StrictMode to prevent double initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {StrictMode} from "react";
+//import {StrictMode} from "react";
 import {ReactKeycloakProvider} from "@react-keycloak/web";
 import {initOptions, keycloak} from "@/utils/keycloak.ts";
 
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        {/*authentication*/}
-        <ReactKeycloakProvider
-            authClient={keycloak}
-            initOptions={initOptions}
-            LoadingComponent={<div className="text-6xl text-center mt-40 text-gray-500">Loading...</div>}
+    // <StrictMode>
+    //authentication
+    <ReactKeycloakProvider
+        authClient={keycloak}
+        initOptions={initOptions}
+        LoadingComponent={<div className="text-6xl text-center mt-40 text-gray-500">Loading...</div>}
         >
 
         <App />
 
         </ReactKeycloakProvider>
-    </StrictMode>
+    // </StrictMode>
 )
